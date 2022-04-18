11fa842ace4b329f58c411311cdf776c5abc12cb
Ana Sofia <alejandro.as@pucp.edu.pe>
2022-04-18 11:22:31 -0500

    History Controller

--- src/main/java/pucp/edu/pe/clase4/controller/HistoryController.java
+++ src/main/java/pucp/edu/pe/clase4/controller/HistoryController.java
@@ -1,16 +1,33 @@
 package pucp.edu.pe.clase4.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import pucp.edu.pe.clase4.repository.DepartmentsRepository;
+import pucp.edu.pe.clase4.repository.EmployeesRepository;
+import pucp.edu.pe.clase4.repository.HistoryRepository;
+import org.springframework.ui.Model;
 
 
 @Controller
 @RequestMapping(value = "/history")
 public class HistoryController {
 
+    @Autowired
+    EmployeesRepository employeesRepository;
+
+    @Autowired
+    HistoryRepository historyRepository;
+
+    @Autowired
+    DepartmentsRepository departmentsRepository;
+
     @GetMapping(value = {"","/"})
-    public String historialEmpleado(){
-        //COMPLETAR
+    public String historialEmpleado(Model model){
+        model.addAttribute("listaHistorial", historyRepository.listaHistorial());
+        model.addAttribute("listaDepartments", departmentsRepository.findAll());
+        model.addAttribute("listaEmployees", employeesRepository.findAll());
+        return "history/lista";
     }
 
 
