f409c4e9523e55c004a7b484f495ece6f21faaad
Ana Sofia <alejandro.as@pucp.edu.pe>
2022-04-18 11:40:24 -0500

    Dtos- History Repository

---
+++ src/main/java/pucp/edu/pe/clase4/dto/DepSalarioDto.java
@@ -0,0 +1,7 @@
+package pucp.edu.pe.clase4.dto;
+
+public interface DepSalarioDto {
+    String getdepname();
+    int getpromedio();
+    int getdepid();
+}
---
+++ src/main/java/pucp/edu/pe/clase4/dto/EmpleadoxDepDto.java
@@ -0,0 +1,9 @@
+package pucp.edu.pe.clase4.dto;
+
+public interface EmpleadoxDepDto {
+    int getemployeeid();
+    String getnombre();
+    String getapellido();
+    String getcargo();
+    int getsueldo();
+}
