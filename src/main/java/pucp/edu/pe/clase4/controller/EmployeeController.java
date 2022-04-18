a84bfb344442509f222552f148886262c174343a
NicoleG <guillen.nicole@pucp.edu.pe>
2022-04-18 11:07:01 -0500

    pom

--- src/main/java/pucp/edu/pe/clase4/controller/EmployeeController.java
+++ src/main/java/pucp/edu/pe/clase4/controller/EmployeeController.java
@@ -1,14 +1,15 @@
 package pucp.edu.pe.clase4.controller;
-import com.example.laboratorio4.entity.Employees;
-import com.example.laboratorio4.repository.DepartmentsRepository;
-import com.example.laboratorio4.repository.EmployeesRepository;
-import com.example.laboratorio4.repository.JobsRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import pucp.edu.pe.clase4.entity.Employees;
+import pucp.edu.pe.clase4.repository.DepartmentsRepository;
+import pucp.edu.pe.clase4.repository.EmployeesRepository;
+import pucp.edu.pe.clase4.repository.JobsRepository;
 
 import javax.jws.WebParam;
 import javax.validation.Valid;
